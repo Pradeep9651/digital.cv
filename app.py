5d5de1a3cbead1428d28d8d185611ede1b128fbe
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2024-05-09 12:47:35 +0530

    lnk

--- app.py
+++ app.py
@@ -16,20 +16,18 @@ PAGE_TITLE = "Digital CV | Pradeep Yadav"
 PAGE_ICON = ":wave:"
 NAME = "Pradeep Yadav"
 DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+Software Developer.
 """
-EMAIL = "yadavpradeep@gmail.com"
+EMAIL = "yadavpradeep2313@gmail.com"
 SOCIAL_MEDIA = {
     "LinkedIn": "https://www.linkedin.com/in/pradeep-yadav-5aa47a20b/",
     "GitHub": "https://github.com/Pradeep9651",
-    "Twitter": "https://twitter.com",
     "leetcode":"https://leetcode.com/u/yadavpradeep2313/"
 }
 PROJECTS = {
     "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
     "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Brain Tumor Detection System-Web app using Python & Streamlit":"https://github.com/Pradeep9651/Brain_tumor"
 }
 
 
